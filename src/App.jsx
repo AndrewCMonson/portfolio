e13fde8d23eebf45b07a8f01d2cb8072504201bf
Andrew Monson <andrewmonsondev@gmail.com>
2024-02-02 09:05:08 -0500

    removed hidden nav on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import Nav from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Projects from './components/Projects.jsx';
 import ContactForm from './components/ContactForm.jsx';
-import useScrollDirection from './hooks/useScrollDirection.js';
 import Footer from './components/Footer.jsx';
 
 const App = () => {
-	const scrollDirection = useScrollDirection();
-
 	return (
 		<>
-			<div
-				className={`sticky ${scrollDirection === 'down' ? '-top-24' : 'top-0'}`}
-			>
+			<div>
 				<Nav />
 			</div>
 			<div className="h-full flex flex-col">
