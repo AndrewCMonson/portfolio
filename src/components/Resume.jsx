8422bc483f2ffad20d6d9a25d417f5c1699fc6c3
Andrew Monson <139920681+AndrewCMonson@users.noreply.github.com>
2024-02-06 19:01:08 -0500

    create resume skeleton
    fleshed out resume format
    need to add animation on load
    need to add new pictures, links and test

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -11,7 +11,16 @@ import {
 
 const Resume = () => {
 	return (
-		<section className='container mx-auto flex justify-center mt-24'>
+		<section className="container mx-auto flex flex-col items-center justify-center mt-16">
+			<div className='flex flex-col items-center'>
+				<Typography variant="h3" color="blue-gray">
+					How&apos;d I get here?
+				</Typography>
+				<Typography variant="h6" color="blue-gray" className="pb-8">
+					(it&apos;s been a ride)
+				</Typography>
+			</div>
+			
 			<div className="w-[32rem]">
 				<Timeline>
 					<TimelineItem>
@@ -25,9 +34,14 @@ const Resume = () => {
 									withBorder
 								/>
 							</TimelineIcon>
-							<Typography variant="h5" color="blue-gray">
-								Timeline Title Here.
-							</Typography>
+							<div className="flex flex-col">
+								<Typography variant="h5" color="blue-gray">
+									Full-stack Web Developer
+								</Typography>
+								<Typography variant="small" color="blue-gray">
+									Freelance/Student
+								</Typography>
+							</div>
 						</TimelineHeader>
 						<TimelineBody className="pb-8">
 							<Typography color="gary" className="font-normal text-gray-600">
@@ -50,9 +64,14 @@ const Resume = () => {
 									withBorder
 								/>
 							</TimelineIcon>
-							<Typography variant="h5" color="blue-gray">
-								Timeline Title Here.
-							</Typography>
+							<div className="flex flex-col">
+								<Typography variant="h5" color="blue-gray">
+									Sr. Talent Advisor
+								</Typography>
+								<Typography variant="small" color="blue-gray">
+									Elevate Digital
+								</Typography>
+							</div>
 						</TimelineHeader>
 						<TimelineBody className="pb-8">
 							<Typography color="gary" className="font-normal text-gray-600">
@@ -65,6 +84,7 @@ const Resume = () => {
 						</TimelineBody>
 					</TimelineItem>
 					<TimelineItem>
+						<TimelineConnector />
 						<TimelineHeader>
 							<TimelineIcon className="p-0">
 								<Avatar
@@ -74,9 +94,44 @@ const Resume = () => {
 									withBorder
 								/>
 							</TimelineIcon>
-							<Typography variant="h5" color="blue-gray">
-								Timeline Title Here.
+							<div className="flex flex-col">
+								<Typography variant="h5" color="blue-gray">
+									Sr. Technical Recruiter
+								</Typography>
+								<Typography variant="small" color="blue-gray">
+									Elevate Digital
+								</Typography>
+							</div>
+						</TimelineHeader>
+						<TimelineBody className='pb-8'>
+							<Typography color="gary" className="font-normal text-gray-600">
+								The key to more success is to have a lot of pillows. Put it this
+								way, it took me twenty five years to get these plants, twenty
+								five years of blood sweat and tears, and I&apos;m never giving
+								up, I&apos;m just getting started. I&apos;m up to something. Fan
+								luv.
 							</Typography>
+						</TimelineBody>
+					</TimelineItem>
+					<TimelineItem>
+						<TimelineConnector />
+						<TimelineHeader>
+							<TimelineIcon className="p-0">
+								<Avatar
+									size="sm"
+									src="https://docs.material-tailwind.com/img/team-3.jpg"
+									alt="user 3"
+									withBorder
+								/>
+							</TimelineIcon>
+							<div className="flex flex-col">
+								<Typography variant="h5" color="blue-gray">
+									Education and Training Program Manager
+								</Typography>
+								<Typography variant="small" color="blue-gray">
+									United States Air Force
+								</Typography>
+							</div>
 						</TimelineHeader>
 						<TimelineBody>
 							<Typography color="gary" className="font-normal text-gray-600">
